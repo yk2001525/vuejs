b32109ef76c6ce6c2857e096335d8d26af36e3dd
”yk2001525“ <1272689242@qq.com>
2021-03-25 20:52:07 +0800

    3月25日更新

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,149 @@
 <template>
-  <h2>分类</h2>
+  <div class="wrapper" ref="aaaa">
+    <ul class="content">
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+      <li>商品</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Category"
-  }
+import BScroll from "better-scroll";
+
+export default {
+  name: "Category",
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  created() {
+    //需要有东西保存它
+    // this.scroll = new BScroll(this.$refs.aaaa,{
+    // })
+  },
+  mounted() {
+    this.scroll = new BScroll(document.querySelector(".wrapper"), {
+      probeType: 3,
+      pullUpLoad: true,
+    });
+    this.scroll.on("scroll", (position) => {
+      console.log(position);
+    });
+    this.scroll.on("pullingUp", () => {
+      console.log("上拉加载更多");
+      setTimeout(() => {
+        this.scroll.finishPullUp();
+      }, 2000);
+    });
+  },
+};
 </script>
 
 <style scoped>
+.wrapper {
+  height: 150px;
+  background-color: red;
 
+  overflow: hidden;
+  /* overflow-y:scroll; */
+}
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,43 +1,155 @@
 <template>
-  <div class="home">
+  <div id="home" class="wrapper">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+   
+    <scroll class="content" ref="scroll" :probe-type="3" 
+    @scroll="contentScroll" :pull-up-load="true" 
+    @pullingUp="loadMore">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control
+        class="tab-control"
+        :titles="['流行', '新款', '精选']"
+        @tabClick="tabClick"
+      ></tab-control>
+      <good-list :goods="showGoods"></good-list>
+    </scroll>
+
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
-  import HomeSwiper from './childComps/HomeSwiper'
-  import {getHomeMultidata} from 'network/home'
-  
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 
- 
-  export default {
-    name: "Home",
-    components:{
-      NavBar,
-      HomeSwiper
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl.vue";
+import { getHomeMultidata, getHomeGoods } from "network/home";
+import GoodList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
+import BackTop from "components/content/backTop/BackTop"
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl,
+    GoodList,
+    Scroll,
+    BackTop
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        //请求数据的模型
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
+      isShowBackTop: false
+    };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
     },
-    data(){
-      return{
-        banners: [],
-        recommends:[]
-      }
-    },
-    created(){
-      //1. 请求多个数据
-      getHomeMultidata().then(res => {
+  },
+  created() {
+    //1. 请求多个数据
+    this.getHomeMultidata();
+    //2. 请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
         // this.result = res;
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
-      })
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+
+        this.$refs.scroll.finishPullUp()
+
+        this.$refs.scroll.scroll.refresh()
+      });
+    },
+    //事件监听相关方法
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    backClick(){
+      this.$refs.scroll.scrollTo(0, 0, 500)
+    },
+    contentScroll(position){
+      this.isShowBackTop = (-position.y) > 1000
+    },
+    loadMore(){
+      this.getHomeGoods(this.currentType)
     }
-  }
+  },
+};
 </script>
 
-<style scoped>  
-  .home-nav{
-    background-color: var(--color-tint);
-    color: white;
-  }
+<style scoped>
+#home {
+  padding-top: 44px;
+  height: 100vh;
+  position: relative;
+}
+.home-nav {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: var(--color-tint);
+  color: white;
+  z-index: 9;
+}
+.tab-control {
+  /* position: sticky; */
+  top: 44px;
+  z-index: 9;
+}
+.content {
+  /* height: 300px; */
+  overflow: hidden;
+
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+/* .content {
+  height: cacl(100% - 93px);
+  overflow: hidden;
+  margin-top: 44px;
+} */
 </style>
